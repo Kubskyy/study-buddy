3171f11bd0fe0ffdcddbb9791ed8d00a6016cd29
Kuba <kubaa.rutkowski123@gmail.com>
2021-09-26 02:16:53 +0200

    Fixing search bar with faker msw database

--- src/mocks/browser.js
+++ src/mocks/browser.js
@@ -1,13 +1,31 @@
 import { setupWorker } from 'msw';
 import { handlers } from './handlers';
 import { db } from 'mocks/db';
+import faker from 'faker';
 
 export const worker = setupWorker(...handlers);
 
-const createStudents = () => {
+const seed = () => {
+  db.group.create({
+    id: 'A',
+  });
+  db.group.create({
+    id: 'B',
+  });
+  db.group.create({
+    id: 'C',
+  });
   for (let i = 0; i < 15; i++) {
     db.student.create();
+    db.event.create();
   }
 };
 
-createStudents();
+seed();
+
+window.mocks = {
+  seed,
+  getStudents: () => db.student.getAll(),
+  getEvents: () => db.event.getAll(),
+  getGroups: () => db.group.getAll(),
+};
